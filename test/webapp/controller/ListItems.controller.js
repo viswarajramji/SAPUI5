0ca5d5ec653a6382a9f170b4730f1cfad45b96d6
viswarajramji <viswarajramji93@gmail.com>
2021-10-15 22:30:02 +0530

    Filter operator

--- test/webapp/controller/ListItems.controller.js
+++ test/webapp/controller/ListItems.controller.js
@@ -1,11 +1,17 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-    "test/controller/formatter"
+    "test/controller/formatter",
+    "sap/m/MessageToast",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ], function(
 	Controller,
 	JSONModel,
-	formatter
+	formatter,
+	MessageToast,
+	Filter,
+	FilterOperator
 ) {
 	"use strict";
 
@@ -26,6 +32,18 @@ sap.ui.define([
             if(searchValue){
                 console.log("test");
             }
-		}
+		},
+
+        search:function(oEvent){
+           var value=oEvent.getSource().getProperty("value");
+           var filter=[];
+           if(value){
+            filter.push(new Filter("ProductName",FilterOperator.EQ,value));
+           }
+          
+           var listItems= this.getView().byId("listItem").getBinding("items")
+           listItems.filter(filter);
+
+        }
 	});
 });
