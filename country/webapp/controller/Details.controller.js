e9a9a2b333ee9668e8d33908a35ff914b0defbb0
viswarajramji <viswarajramji93@gmail.com>
2021-10-29 06:38:58 +0400

    29 oct 2021 - sort and messagbox

--- country/webapp/controller/Details.controller.js
+++ country/webapp/controller/Details.controller.js
@@ -6,5 +6,20 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("country.controller.Details", {
+        /**
+         * @override
+         */
+        onInit: function() {
+            this._oRouter=this.getOwnerComponent().getRouter();
+            this._oRouter.getRoute("detail").attachPatternMatched(this._attachRoutePatterMatched,this);
+        },
+        _attachRoutePatterMatched:function(oEvent){
+          this.layout= oEvent.getParameter("arguments").layout;
+          this.productId=oEvent.getParameter("arguments").productId || this.productId || "0";
+          this.getView().bindElement({
+              path:"/countryList/"+this.productId,
+              model:"country"
+          });
+        }
 	});
 });
--- country/webapp/controller/Master.controller.js
+++ country/webapp/controller/Master.controller.js
@@ -2,12 +2,16 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
-	"sap/f/library"
+	"sap/f/library",
+	"sap/ui/model/Sorter",
+	"sap/m/MessageBox"
 ], function(
 	Controller,
 	Filter,
 	FilterOperator,
-	fioriLibrary
+	library,
+	Sorter,
+	MessageBox
 ) {
 	"use strict";
 
@@ -19,6 +23,7 @@ sap.ui.define([
 		onInit: function() {
 		
 			this._oRouter=this.getOwnerComponent().getRouter();
+			this._descendingSort=false;
 
 		},
 		//TODO : and / or operator in this list
@@ -35,9 +40,24 @@ sap.ui.define([
 		},
 
 		countryDetails:function(oEvent){
-
+			var id=oEvent.getSource().getBindingContextPath().split("/")[2]
 			this._oRouter.navTo("detail",{ 
-				layout: fioriLibrary.LayoutType.TwoColumnsMidExpanded
+				layout: fioriLibrary.LayoutType.TwoColumnsMidExpanded,
+				productId:id
+			});
+		},
+
+		sortCountry:function(){
+			this._descendingSort=!this._descendingSort;
+			var oSorter=new Sorter("countryName",this._descendingSort);
+			var oTable=this.getView().byId("masterTable");
+			var oItems=oTable.getBinding("items");
+			oItems.sort(oSorter)
+		},
+
+		addCountry:function(){
+			MessageBox.information("feature Not Implmenet",{
+				title: "Information"
 			});
 		}
 	});
